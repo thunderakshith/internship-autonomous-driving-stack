e4ef21f4d60a9d3cf733daf9a62b5b447b1e95c1
thunderakshith <thunderakshith@gmail.com>
2026-02-02 17:25:10 +0530

    Add basic perception module

---
+++ src/perception.py
@@ -0,0 +1,9 @@
+import cv2
+
+image = cv2.imread("data/test_road.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 100, 200)
+
+cv2.imshow("Perception - Edge Detection", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
